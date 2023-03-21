b54b9a2b5a983f820820ca55aa38e6dfb5d44c35
himawari00 <n9343226@gmail.com>
2023-03-21 10:46:46 +0900

    환경세팅 완료

---
+++ src/main/java/com/jsj/blog/test/BlogControllerTest.java
@@ -0,0 +1,17 @@
+package com.jsj.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//스프링이 com.example.demo 패키지 이하를 스캔해서 
+//특정 어노테이션이 붙어있는 클래스 파일들을 new해서(IOC)스프링 컨테이너에 관리해줌
+@RestController 
+public class BlogControllerTest {
+	
+	//http://localhost:8080/test/hello
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>hello spring boot</h1>";
+	}
+}
